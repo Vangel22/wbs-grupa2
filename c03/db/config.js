85d0ef8376f8db67898a4f0393ae4bc0205a8b06
Vangel <69527726+Vangel22@users.noreply.github.com>
2025-03-20 20:18:49 +0100

    Update config.js

--- c03/db/config.js
+++ c03/db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://Vangel22:test1234@cluster0.12jzasd.mongodb.net/grupa-4007?retryWrites=true&w=majority";
+  "";
 
 async function connect() {
   try {
