1fb461a90559bb5b2e1a5e7c9189bf13f9026bec
Vangel22 <h.vangel22@gmail.com>
2025-03-20 21:33:18 +0100

    Save class

--- c03/handlers/accounts.js
+++ c03/handlers/accounts.js
@@ -1,15 +1,56 @@
-const getAllAccounts = async () => {
+const {
+  getAccounts,
+  createAccount,
+  updateAccount,
+  removeAccount,
+} = require("../models/accounts");
+const {
+  validateAccount,
+  AccountCreate,
+  AccountUpdate,
+} = require("../models/validate");
+
+const getAllAccounts = async (req, res) => {
   try {
+    const accounts = await getAccounts();
+    return res.status(200).send(accounts);
   } catch (err) {
     return res.status(500).send("Invalid server error!");
   }
 };
 
-const createNewAccount = async () => {};
+const createNewAccount = async (req, res) => {
+  try {
+    await validateAccount(req.body, AccountCreate);
+    await createAccount(req.body);
+    return res.status(200).send("Account created successfully!");
+  } catch (err) {
+    return res.status(500).send(err ? err.error : "Invalid server error!");
+  }
+};
 
-const updateCurrentAccount = async () => {};
+const updateCurrentAccount = async (req, res) => {
+  try {
+    await validateAccount(req.body, AccountUpdate);
+    await updateAccount(req.params.id, req.body);
+    return res
+      .status(200)
+      .send(`Account with id ${req.params.id} updated successfully!`);
+  } catch (err) {
+    return res.status(500).send(err ? err.error : "Invalid server error!");
+  }
+};
 
-const removeCurrentAccount = async () => {};
+const removeCurrentAccount = async (req, res) => {
+  try {
+    await removeAccount(req.params.id);
+    return res
+      .status(200)
+      .send(`Account with id ${req.params.id} deleted successfully!`);
+  } catch (err) {
+    return res.status(500).send("Invalid server error!");
+  }
+};
 
 module.exports = {
   getAllAccounts,
--- c03/index.js
+++ c03/index.js
@@ -1,8 +1,26 @@
 // Infrastrukturen kod
 const express = require("express");
 
+// const connectToDB = require("./db/config");
+// connectToDB();
+
+// Curry function
+require("./db/config")();
+
+const {
+  getAllAccounts,
+  createNewAccount,
+  updateCurrentAccount,
+  removeCurrentAccount,
+} = require("./handlers/accounts");
+
 const app = express();
 
 app.use(express.json());
+
+app.get("/accounts", getAllAccounts);
+app.post("/accounts", createNewAccount);
+app.put("/accounts/:id", updateCurrentAccount);
+app.delete("/accounts/:id", removeCurrentAccount);
 
 app.listen(3000, () => console.log("Server is started at port 3000!"));
