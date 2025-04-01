e7324f4b7a331dbb6acfa3230a06b320cdc93479
Vangel22 <h.vangel22@gmail.com>
2025-04-01 20:12:02 +0200

    Save class

---
+++ c06/models/books/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Cekor 1: Napravete ja semata za ovie polinja
+// title
+// author
+// language
+// pages
+// genre
+// publicationDate
+// publisher
+// createdAt
+// updatedAt
+
+//Cekor 2: Napravete CRUD za ovoj model/schema
