4f26074ece34a94d67e1bc38758624005b8ff361
Vangel22 <h.vangel22@gmail.com>
2025-04-01 20:46:10 +0200

    Save model for books

--- c06/models/books/index.js
+++ c06/models/books/index.js
@@ -11,4 +11,70 @@ const mongoose = require("mongoose");
 // createdAt
 // updatedAt
 
+const bookSchema = mongoose.Schema(
+  {
+    title: String,
+    author: String,
+    language: String,
+    pages: Number,
+    genre: String,
+    publicationDate: Date,
+    publisher: String,
+
+    // book: {
+    //     type: mongoose.SchemaTypes.ObjectId,
+    //     ref: "Book"
+    // }
+  },
+  { timestamps: true }
+);
+
 //Cekor 2: Napravete CRUD za ovoj model/schema
+
+const BookModel = mongoose.model("Book", bookSchema, "books");
+
+// get
+const getBooks = async () => {
+  return await BookModel.find();
+};
+
+// create
+const createBook = async (data) => {
+  const newBook = new BookModel(data);
+  return await newBook.save();
+};
+
+// update
+const updateBook = async (id, data) => {
+  return await BookModel.updateOne({ _id: id }, data);
+};
+
+// remove
+const removeBook = async (id) => {
+  return await BookModel.deleteOne({ _id: id });
+};
+
+module.exports = {
+  getBooks,
+  createBook,
+  updateBook,
+  removeBook,
+};
+
+// class BookModelClass {
+//     constructor(data) {
+
+//     }
+// }
+
+// new BookModelClass(data)
+
+// bookObject = {
+//     title: "Animal Farm",
+//     author: "George Orwel",
+//     language: "English",
+//     pages: 130,
+//     genre: "Political",
+//     publicationDate: "1943-01-01",
+//     publisher: "TRI",
+// }
